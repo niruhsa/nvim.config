2317d0dff746b98062873fd8b2e8911a8b7cf80f
niruhsa <48173574+niruhsa@users.noreply.github.com>
2026-07-19 22:08:44 +1000

    add lazygit to nvim config

--- init.lua
+++ init.lua
@@ -13,6 +13,15 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 require("nvim-tree").setup()
 vim.keymap.set('n', '<leader>tt', '<Cmd>NvimTreeToggle<CR>', { desc = 'Toggle Tree' })
+-- On startup, if there is no file opened with neovim, open tree by default
+vim.api.nvim_create_autocmd("VimEnter", {
+  once = true,
+  callback = function()
+    if vim.fn.argc() == 0 then
+      require("nvim-tree.api").tree.open()
+    end
+  end,
+})
 
 -- Telescope setup
 local builtin = require('telescope.builtin')
@@ -20,6 +29,9 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- LazyGit setup
+vim.keymap.set('n', '<leader>gg', '<Cmd>LazyGit<CR>', { desc = 'LazyGit' })
 
 -- ===== Language Configuration =====
 -- === Go ===
